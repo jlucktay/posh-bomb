f63d5aefbb7158831ce05bb7629daeea967ddb8d
James Lucktaylor <jlucktay@gmail.com>
2016-12-04 12:15:10 +0000

    Added more Pester. 100% coverage on 1 file! \o/

--- GiantBomb/Convert-GameUrlForApi.Tests.ps1
+++ GiantBomb/Convert-GameUrlForApi.Tests.ps1
@@ -13,13 +13,23 @@ Describe "Convert-GameUrlForApi" {
             Should Throw
     }
 
+    It "only accepts well-formed URLs" {
+        { Convert-GameUrlForApi -Url "http/www.google" } |
+            Should Throw
+    }
+
     It "only accepts Giant Bomb game URLs" {
         { Convert-GameUrlForApi -Url "http://www.google.com.au" } |
             Should Throw
     }
 
+    It "returns null when appropriate" {
+        Convert-GameUrlForApi -Url "http://www.giantbomb.com/hitman/3030-abc/" |
+            Should Be $null
+    }
+
     It "converts a game URL accurately" {
         Convert-GameUrlForApi -Url "http://www.giantbomb.com/hitman/3030-45150/" |
-            Should Be "http://www.giantbomb.com/api/game/3030-45150/"
+            Should BeExactly "http://www.giantbomb.com/api/game/3030-45150/"
     }
 }
--- GiantBomb/Convert-GameUrlForApi.ps1
+++ GiantBomb/Convert-GameUrlForApi.ps1
@@ -4,14 +4,14 @@ Set-StrictMode -Version Latest
 function Convert-GameUrlForApi {
     param(
         [Parameter(Mandatory=$true)]
-        [ValidatePattern('^http:\/\/www.giantbomb.com\/[a-z0-9\-]+\/3030-[0-9]+\/$')]
+        [ValidatePattern('^http:\/\/www.giantbomb.com\/[a-z0-9\-]+\/3030-')]
         [ValidateScript({[Uri]::IsWellFormedUriString($($_), [UriKind]::Absolute)})]
         [String]$Url
     )
 
-    Clear-Variable -Name Matches
-
     if ($Url -match "\/3030\-[0-9]+\/$") {
         return "http://www.giantbomb.com/api/game$($Matches[0])"
+    } else {
+        return $null
     }
 }
