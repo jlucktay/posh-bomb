6cafb0764b6792c47201d0bb43dc95c3fc3cebb4
James Lucktaylor <jlucktay@gmail.com>
2016-12-05 18:33:22 +0000

    Added tests for Confirm-DownloadChoice

---
+++ GiantBomb/Confirm-DownloadChoice.Tests.ps1
@@ -0,0 +1,21 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Confirm-DownloadChoice" {
+    It "requires the VideoName parameter" -Skip {
+        Confirm-DownloadChoice -Confirm:$false |
+            Should Throw
+    }
+
+    It "requires a value for the VideoName parameter" {
+        { Confirm-DownloadChoice -VideoName } |
+            Should Throw
+    }
+
+    It "returns false when SkipConfirm is set" {
+        $SkipConfirm = $true
+        Confirm-DownloadChoice -VideoName "random name" |
+            Should Be $false
+    }
+}
