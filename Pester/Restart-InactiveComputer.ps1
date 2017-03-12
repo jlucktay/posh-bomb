45576f084ae126c88ff78a6cc9d4b82930f54a59
James Lucktaylor <jlucktay@gmail.com>
2017-03-12 21:28:32 +0000

    Removed irrelevant Pester practice scripts

--- Pester/Restart-InactiveComputer.ps1
+++
@@ -1,8 +0,0 @@
-function Restart-InactiveComputer {
-    # [CmdletBinding(SupportsShouldProcess=$true)]
-    # param()
-
-    if (-not (Get-Process explorer -ErrorAction SilentlyContinue)) {
-        Restart-Computer -Force
-    }
-}
